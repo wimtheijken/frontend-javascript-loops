7a780cc086746060f7ed83bdea6d3a2ca0f1d77a
novaeeken <nova_eeken@hotmail.com>
2023-03-21 11:50:55 +0100

    Update assignment

--- loops.js
+++ loops.js
@@ -1,15 +1,22 @@
 // ==========================================
-// Opdracht 1. Maak een for-loop die 3 keer "Hoera!" in de terminal logt
+// Opdracht 1. Maak een for loop die het volgende patroon in de terminal print:
 // Verwachte uitkomsten:
-// Hoera!
-// Hoera!
-// Hoera!
+// *
+// **
+// ***
+// ****
+// *****
+
+// Tip: je kunt de .repeat() methode gebruiken om een karakter een n aantal keer te herhalen...
+// Dit heb je nog niet geleerd, maar bekijk hiervoor dit MDN artikel eens: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
 // ==========================================
 
 
 
+
 // ==========================================
-// Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de vijfde loop het woord "klaar!"
+// Opdracht 2. Maak een for-loop die 4 keer het woord "loop..." logt, en bij de laatste (vijfde) loop het woord "klaar!"
+// Zorg ervoor dat dit ook werkt als we het aantal herhalingen in de for-loop zouden aanpassen...
 // Verwachte uitkomsten:
 // loop...
 // loop...
@@ -20,23 +27,9 @@
 
 
 
-// ==========================================
-// Opdracht 3. Maak een for-loop die van 0 tot 15 loopt. Voor ieder getal wordt in de console gelogd of het getal even of oneven is.
-// Verwachte uitkomsten:
-// 0 is even
-// 1 is oneven
-// 2 is even
-// 3 is oneven
-// .... etc.
-// 15 is oneven
-
-// Tip: een getal is oneven als je het door 2 deelt en er blijft nog iets over. Een getal is even als je het deelt door 2 en er blijft niets over. Google daarom eens naar 'mdn webdocs remainder operator'.
-// ==========================================
-
-
 
 // ==========================================
-// [BONUSOPDRACHT] - optioneel: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
+// Opdracht 3: maak een for-loop die van 0 tot 9 loopt en de getallen 0 tot 9 logt.
 // Echter, vanaf het getal 3 komt er een > voor ieder getal te staan, en vanaf het getal 6 komt er >> voor ieder getal te staan.
 // Verwachte uitkomsten:
 // 0
@@ -51,3 +44,43 @@
 // >> 9
 // ==========================================
 
+
+
+
+// ==========================================
+// Opdracht 4 (BONUS): schrijf een for-loop die van 0 tot 100 loopt en de getallen print.
+// Echter, voor veelvouden van 3 print je "Fizz" in plaats van het nummer, en voor veelvouden van 5 print je "Buzz".
+// Voor getallen die zowel een veelvoud van 3 als van 5 zijn, print je "FizzBuzz"
+// Verwachte uitkomsten:
+// 1
+// 2
+// Fizz
+// 4
+// Buzz
+// Fizz
+// 7
+// 8
+// Fizz
+// Buzz
+// 11
+// Fizz
+// 13
+// 14
+// FizzBuzz
+// 16
+// 17
+// Fizz
+// 19
+// Buzz
+// Fizz
+// 22
+// 23
+// Fizz
+// Buzz
+// 26
+// Fizz
+// 28
+// 29
+// FizzBuzz
+// etc.
+// ==========================================
